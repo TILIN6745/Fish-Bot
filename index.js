dce384be829027977921ef65bc646025597359d5
Tilinn <jm293229@gmail.com>
2024-11-07 19:52:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('CapiPremium\nBot', {
+say('Fish Bot\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Anthony Ventas`, {
+say(`Bot creado por TILIN`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
